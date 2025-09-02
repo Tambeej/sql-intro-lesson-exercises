47dbde92b25a87fe88aec3a595d2e669b2dea395
Tambeej <bejella@gmail.com>
2025-09-02 08:54:17 +0300

    ex2 done

--- solutions/ex_1.sql
+++ solutions/ex_1.sql
@@ -11,10 +11,13 @@ USE sql_intro;
 -- INSERT INTO Dolphin VALUES ( "Daron", "blue",  1, b'1');
 
 
-INSERT INTO Dolphin VALUES ("Luna", "Gray", 2, b'0');
-INSERT INTO Dolphin VALUES ("Aqua", "Silver", 3, b'1');
+-- INSERT INTO Dolphin VALUES ("Luna", "Gray", 2, b'0');
+-- INSERT INTO Dolphin VALUES ("Aqua", "Silver", 3, b'1');
 
--- Using default healthy (true)
-INSERT INTO Dolphin (name, color, height) VALUES ("Misty", "Pink", 2);
-INSERT INTO Dolphin (name, color, height) VALUES ("Wave", "green", 4);
-INSERT INTO Dolphin (name, color, height) VALUES ("Echo", "Blue", 3);
+-- INSERT INTO Dolphin (name, color, height) VALUES ("Misty", "Pink", 2);
+-- INSERT INTO Dolphin (name, color, height) VALUES ("Wave", "green", 4);
+-- INSERT INTO Dolphin (name, color, height) VALUES ("Echo", "Blue", 3);
+
+SELECT *
+FROM Dolphin
+WHERE height>2;
