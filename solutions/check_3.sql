0758ed6791d3f15009e33c60b2857d7d27d73226
Tambeej <bejella@gmail.com>
2025-09-02 08:51:23 +0300

    all chechk spots and ex1 done

--- solutions/check_3.sql
+++ solutions/check_3.sql
@@ -1,1 +1,2 @@
 USE sql_intro;
+SELECT coolness, name,mythology FROM Deity WHERE coolness>=8 and mythology = "greek";
--- solutions/ex_1.sql
+++ solutions/ex_1.sql
@@ -1,1 +1,20 @@
 USE sql_intro;
+
+-- CREATE TABLE Dolphin(
+--     name VARCHAR(50) PRIMARY KEY,
+--     color VARCHAR(20),
+--     height INT,
+--     healthy BIT
+-- )
+-- ALTER TABLE Dolphin
+-- MODIFY healthy BIT DEFAULT b'1';
+-- INSERT INTO Dolphin VALUES ( "Daron", "blue",  1, b'1');
+
+
+INSERT INTO Dolphin VALUES ("Luna", "Gray", 2, b'0');
+INSERT INTO Dolphin VALUES ("Aqua", "Silver", 3, b'1');
+
+-- Using default healthy (true)
+INSERT INTO Dolphin (name, color, height) VALUES ("Misty", "Pink", 2);
+INSERT INTO Dolphin (name, color, height) VALUES ("Wave", "green", 4);
+INSERT INTO Dolphin (name, color, height) VALUES ("Echo", "Blue", 3);
